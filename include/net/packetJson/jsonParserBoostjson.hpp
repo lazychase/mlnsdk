17eda469fe47060378ddf1b968a02b0d7f752fc9
lazychase <97491125+lazychase@users.noreply.github.com>
2022-04-26 14:06:52 +0900

    Create jsonParserBoostjson.hpp
    boost json

---
+++ include/net/packetJson/jsonParserBoostjson.hpp
@@ -0,0 +1,30 @@
+#include <exception>
+#include <string>
+#include <tuple>
+
+#include <boost/json.hpp>
+#include <iostream>
+
+
+namespace mln::net::boostjson {
+	static std::tuple<bool, boost::json::value, std::string> parse(unsigned char* body, uint32_t bodySize, bool getUrlString) {
+
+		try {
+			std::string jsonBodyString{ (char*)body, bodySize };
+
+			if (!getUrlString) {
+				return { true, boost::json::parse(jsonBodyString), "" };
+			}
+			else {
+				boost::json::value jsonObj = boost::json::parse(jsonBodyString);
+				std::string urlString = boost::json::value_to<std::string>(jsonObj.at("url"));
+
+				return { true, jsonObj.as_object()["body"], urlString };
+			}
+		}
+		catch (std::exception e) {
+			std::cout << "failed parse(). msg:" << e.what() << std::endl;
+			return { false, boost::json::value(nullptr), "" };
+		}
+	}
+}//namespace mln::net::boostjson {
